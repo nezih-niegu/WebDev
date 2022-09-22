1a3dff522a3c188e913383756e24d5cfbe7053d2
Nezih Nieto Gutiérrez <100229436+nezih-niegu@users.noreply.github.com>
2022-09-21 21:26:22 -0500

    Add files via upload

--- P1/script.js
+++ P1/script.js
@@ -1,5 +1,5 @@
 function getData(){
-    url = "https://www.inegi.org.mx/app/api/indicadores/desarrolladores/jsonxml/INDICATOR/1002000001/es/01/false/BISE/2.0/02c49f7d-32df-6048-4648-673e3542b7f8?type=json";
+    url = "https://api2.binance.com/api/v3/ticker/24hr";
     console.log(url);
     var request = new XMLHttpRequest();
     request.onload = function(bg) {
@@ -7,28 +7,23 @@ function getData(){
       console.log(bg.target.response);
       var data = JSON.parse(bg.target.response);
       console.log(data);
-      var date = document.getElementById("date");
-  
-      date.innerHTML = data.Series[0].LASTUPDATE;
-      console.log(data);
       createTable(data);
     }
     request.open("GET", url, true);
-    request.send();//ERROR
+    request.send();
   }
 
 function createTable(data)
   {
-  cn = 7;
-  index=0;
-   while(data.Series[0].OBSERVATIONS[index]){
-     var x=document.getElementById('myTable').insertRow(r);
-     for(var c=0;c<cn;c++)  
-      {
-       var y=  x.insertCell(c);
-       y.innerHTML= data.Series[0].OBSERVATIONS[index]; 
-      }
-      r+=1;
-      index+=1;
+   index=0;
+   while(data[index]&&index<40){
+       var x=document.getElementById('myTable').insertRow(index+1);
+       var y =  x.insertCell(0);
+       y.innerHTML= data[index].symbol;
+       var y =  x.insertCell(1);
+       y.innerHTML= data[index].lastPrice;
+       var y =  x.insertCell(2);
+       y.innerHTML= data[index].priceChangePercent;
+       index+=1;
      }
   }
